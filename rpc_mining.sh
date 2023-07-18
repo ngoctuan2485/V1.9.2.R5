36da9f45de3c911852cc2ef7d04c2b11e3992c8a
ngoctuan2485 <139063228+ngoctuan2485@users.noreply.github.com>
2023-07-18 21:58:02 +0700

    rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -4,3 +4,4 @@ while :; do
     ./astrominer -w dero1qyz29z9rjkhpmh4gxefdl9sy03p4k34h3w4zwm3szjzn5cs6mkvfuqq5mfjlr -r dero.androidmining.xyz:10100 -r1 nodent2.cpumining.cloud:10100 -m 6 -p rpc
     sleep 5;
 done
+
